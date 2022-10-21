66aa0933e541984e076a70d9f23abe59b09a6d6e
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2022-10-21 15:39:44 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/button';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -7,6 +8,8 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>testando se funciona</Text>
+      <Button />
     </View>
   );
 }
