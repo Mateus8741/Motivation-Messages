0c97035ae145c546887706a59fe9bc50e70ada77
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2022-10-24 14:53:48 -0300

    att

--- App.tsx
+++ App.tsx
@@ -1,13 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { api } from "@/services/api";
+
+
+interface Pokemons {
+  name: string;
+}
 
 export default function App() {
+  const [data, setData] = useState<Pokemons>();
+
+  useEffect(() => {
+    api.get("/pokemon/type").then((response) => {
+      setData(response.data);
+    });
+    
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
       <Text>testando se funciona</Text>
+      <Text>{data?.name}</Text>
     </View>
   );
 }
